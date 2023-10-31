45c92680acda417accd60c4121ef49b0d62335ac
krishnababukr <148951234+krishnababukr@users.noreply.github.com>
2023-10-31 16:22:56 +0530

    Create forloop.java

---
+++ forloop.java
@@ -0,0 +1,14 @@
+package corejavaprogram;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		
+		for(int count=1;count <=20;count++) {
+			
+			System.out.println("This is iteration :"+count);
+		}
+
+	}
+
+}
