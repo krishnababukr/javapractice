de421ea6e9b52488aa2e5ad98c093ce4be9a66d4
krishnababukr <148951234+krishnababukr@users.noreply.github.com>
2023-10-31 16:15:31 +0530

    Create whileloop.java

---
+++ whileloop.java
@@ -0,0 +1,17 @@
+package corejavaprogram;
+
+public class loopprogram {
+
+	public static void main(String[] args) {
+		int count = 1;
+
+		while (count <= 10)
+			;
+		{
+			System.out.println("this is iteration :" + count);
+			count++;
+		}
+
+	}
+
+}
