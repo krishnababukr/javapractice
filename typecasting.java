6723b6370d8a4cfc4761eeebfd35ca266446851c
krishnababukr <148951234+krishnababukr@users.noreply.github.com>
2023-10-31 16:11:55 +0530

    Create typecasting.java

---
+++ typecasting.java
@@ -0,0 +1,51 @@
+package corejavaprogram;
+
+public class Datatypesdemo {
+
+	public static void main(String[] args) {
+		byte bytevalue =127;
+		
+		short shortvalue =32767;
+		
+		int intvalue =1234589;
+		
+		long longvalue =99999999967432355L;
+		
+		float floatvalue =3.14f;
+		
+		double doublevalue =3.14566;
+		
+		boolean booleanvaluetrue =true;
+		
+		boolean booleanvaluefalse =false;
+		
+		char charvalue ='A';
+		
+		System.out.println("byte value " +bytevalue);
+		
+		System.out.println("short value " +shortvalue);
+		
+		System.out.println("int value " +intvalue);
+		
+		System.out.println("long value " +longvalue);
+		
+		System.out.println("float value " +floatvalue);
+		
+		System.out.println("double value " +doublevalue);
+		
+		System.out.println("boolean value " +true);
+		
+		System.out.println("boolean value " +false);
+		
+		System.out.println("char value " +charvalue);
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
