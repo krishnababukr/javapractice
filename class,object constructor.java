f9ccf8176043169abfdef91301ecf09e2451bedc
krishnababukr <148951234+krishnababukr@users.noreply.github.com>
2023-11-01 14:11:26 +0530

    Create class,object constructor.java

---
+++ class,object constructor.java
@@ -0,0 +1,24 @@
+public class Person {
+    private String name;
+    private int age;
+
+    
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void greet() {
+        System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
+    }
+
+    public static void main(String[] args) {
+        // Creating objects of the Person class
+        Person person1 = new Person("Alice", 25);
+        Person person2 = new Person("Bob", 30);
+
+        
+        person1.greet(); 
+        person2.greet(); 
+    }
+}
